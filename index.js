17aa735d74db0ae581e8a0ce108d915b673d8138
Aleksey Smirnov <i@smirnov-alexsey.ru>
2023-01-07 16:02:10 +0300

    trivial routing

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
+app.all('hello', (req, res, next) => {
+    console.log('ALL');
+    next();
+});
+
+const cb = (req, res, next) => {
+    console.log('CB');
+    next();
+};
+
+app.route('/user')
+.get('/hello', cb, (req, res) => {
     res.send('Привет!');
+})
+.post('/hello', cb, (req, res) => {
+    res.send('Привет POST!');
 });
 
 app.listen(port, () => {
