76c9e473639dec666cfb3bd2700b8b5c3f4914ef
Aleksey Smirnov <i@smirnov-alexsey.ru>
2023-01-08 18:49:38 +0300

    add e2e tests

--- src/main.ts
+++ src/main.ts
@@ -31,14 +31,14 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<App>(TYPES.Application).to(App);
 });
 
-function main(): IMainReturn {
+async function main(): Promise<IMainReturn> {
 	const appContainer = new Container();
 	appContainer.load(appBindings);
 
 	const app = appContainer.get<App>(TYPES.Application);
-	app.init();
+	await app.init();
 
 	return { appContainer, app };
 }
 
-export const { appContainer, app } = main();
+export const mainApp = main();
